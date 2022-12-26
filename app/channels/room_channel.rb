fc727ebc98d10ab332b7b82e167ab18f4ad414e5
tamashiro-syuta <syuta5825@gmail.com>
2022-12-26 23:58:34 +0900

    💡 それぞれのメソッドが行なっていることを記述

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,13 +1,16 @@
 # WebSocket処理のサーバーサイドを受け持つファイル
 class RoomChannel < ApplicationCable::Channel
+  # 購読後に呼ばれるメソッド
   def subscribed
     # stream_from "some_channel"
   end
 
+  # 購読解除後に呼ばれるメソッド
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
+  # clientから呼び出された時に呼ばれるメソッド
   def speak
   end
 end
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -3,19 +3,23 @@
 import consumer from "./consumer"
 
 consumer.subscriptions.create("RoomChannel", {
+  // 接続時に呼ばれるコールバック
   connected() {
     // Called when the subscription is ready for use on the server
   },
 
+  // 接続解除時に呼ばれるコールバック
   disconnected() {
     // Called when the subscription has been terminated by the server
   },
 
+  // サーバーからのデータを受信した時に呼ばれるコールバック
   received(data) {
     // Called when there's incoming data on the websocket for this channel
   },
 
-  speak: function() {
+  speak: function () {
+    // Room channelクラスのspeakメソッドをWebSocket経由で呼び出している
     return this.perform('speak');
   }
 });
