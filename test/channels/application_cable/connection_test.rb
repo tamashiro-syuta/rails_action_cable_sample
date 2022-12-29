13310e07d4663be6d5a54c5201b65e3f38d90e55
tamashiro-syuta <syuta5825@gmail.com>
2022-12-29 12:43:50 +0900

    ✅ テストを追加

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,11 +1,24 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
-  #   cookies.signed[:user_id] = 42
-  #
-  #   connect
-  #
-  #   assert_equal connection.user_id, "42"
-  # end
+  # 作成されたコネクションの認証ユーザー情報がcookieのユーザー情報と一致するかテスト
+  test "connection accepts" do
+    # ユーザーの作成し、cookieにidを代入
+    user = User.first
+    cookies.signed[:user_id] = user.id
+
+    # コネクションを接続(これでConnectionオブジェクトが取得できるようになる)
+    connect
+
+    # (identifired_byで作成された)current_userメソッドを呼び出して、上でセットしたユーザーIDと同じか検証
+    assert_equal connection.current_user.id, user.id
+  end
+
+  # 認証エラーになるパターンをテスト
+  test "connection rejects" do
+    # cookieをnilに設定
+    cookies.signed[:user_id] = nil
+    # cookieにユーザーIDがない状態で、connect(コネクションの作成)をすると、接続ができないことを検証
+    assert_reject_connection { connect }
+  end
 end
--- test/channels/room_channel_test.rb
+++ test/channels/room_channel_test.rb
@@ -1,8 +1,26 @@
 require "test_helper"
 
 class RoomChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
+  test "subscribes" do
+    # テスト用に購読者作成するをシュミレート
+    subscribe
+    # subscriptionメソッドでRoomChannelオブジェクトを取得できるので、confirmメソッドで正しく購読できているかを確認
+    assert subscription.confirmed?
+    # 引数と同じ名前のstreamが作成されているかを確認
+    assert_has_stream "room_channel"
+  end
+
+  test 'braodcast' do
+    subscribe
+    text = 'hello'
+    braodcast_text = ApplicationController.render(
+      partial: "messages/message",
+      locals: { message: Message.new(content: text)}
+    )
+
+    # broadcast用のヘルパーメソッドを使って、speakメソッドを呼び出し、私た文字列(text)を含むHTMLがブロードキャストされたか確認
+    assert_broadcast_on('room_channel', message: braodcast_text) do
+      perform :speak, message: text
+    end
+  end
 end
